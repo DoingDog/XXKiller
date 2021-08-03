909ea6e3a9c3829c837fd05f73bfa708944dd03b
ctrl <aqvm@qq.com>
2021-08-03 19:24:14 +0800

    Update mio.bat

--- RMaker/aa/mio.bat
+++ RMaker/aa/mio.bat
@@ -1,7 +1,5 @@
-@echo off
 ren awk.bat gawk.exe
-@gawk "!a[$0]++" mgd.txt>m-ad-mg.txt
+@gawk "!a[$0]++" mgd.txt>what.txt
 ren gawk.exe awk.bat
-ren m-ad-mg.txt what.txt
 del /f /q mgd.txt
 call mix.bat
