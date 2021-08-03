8091bfb29671cc2196a984b6799bffa4ddb1a3d1
ctrl <aqvm@qq.com>
2021-08-03 14:41:24 +0800

    Update add.bat

--- RMaker/aa/add.bat
+++ RMaker/aa/add.bat
@@ -1,5 +1,5 @@
 echo ! Version: %date%>>tempdate.txt
 echo ! Last modified: %date%T%time%Z>>tempdate.txt
-copy tempdate.txt+w.txt ..\w.txt
+copy tempdate.txt+w.txt ..\..\w.txt
 del /f /q *.txt
 exit
