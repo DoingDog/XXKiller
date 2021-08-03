054e83f393c9345f24026447a61c02afcd87b4c5
ctrl <aqvm@qq.com>
2021-08-03 19:22:17 +0800

    Update mix.bat

--- RMaker/aa/mix.bat
+++ RMaker/aa/mix.bat
@@ -1,9 +1,5 @@
-@echo off
 set file=what.txt
 (FOR /F "eol=# delims=" %%i in (%file%) do (echo %%i))>%file%.tmp
-type %file%.tmp>%file%
-del %file%.tmp
-(FOR /F "eol=! delims=" %%i in (%file%) do (echo %%i))>%file%.tmp
-type %file%.tmp>%file%
+(FOR /F "eol=! delims=" %%i in (%file%.tmp) do (echo %%i))>%file%
 del %file%.tmp
 call miz.bat
