1cc4a7ba4d7a16037fa55c4a72fa9dd8000733f8
ctrl <aqvm@qq.com>
2021-08-03 10:46:36 +0800

    Add files via upload

--- RMaker/aa/add.bat
+++ RMaker/aa/add.bat
@@ -1,5 +1,4 @@
 echo [Adblock Plus 2.0]>tempdate.txt
-
 echo ! Version: %date%>>tempdate.txt
 echo ! Last modified: %date%T%time%Z>>tempdate.txt
 copy tempdate.txt+w.txt ..\w.txt
--- RMaker/aa/mib.bat
+++ RMaker/aa/mib.bat
@@ -1,6 +1,4 @@
-
 ! Title: XXKillAds
 ! Homepage: https://github.com/DoingDog/XXKiller
 ! by: XXKiller
 ! License: https://github.com/DoingDog/XXKiller/blob/main/LICENSE
-
