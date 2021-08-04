d22f646ecfc163764c2776a97eb01bf0daa616fa
ctrl <aqvm@qq.com>
2021-08-04 11:04:15 +0800

    Update mia.bat

--- RMaker/aa/mia.bat
+++ RMaker/aa/mia.bat
@@ -1,5 +1,5 @@
 ren get.bat wget.exe
-::start
+::start don
 @echo off
 wget https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
 del /f /q *hsts
@@ -23,6 +23,12 @@ ren lit* i6.txt
 wget https://anti-ad.net/easylist.txt
 del /f /q *hsts
 ren easylist.txt i7.txt
+wget https://raw.githubusercontent.com/BlueSkyXN/AdGuardHomeRules/master/manhua.txt
+del /f /q *hsts
+ren man*.txt i8.txt
 ::新的规则
+::wget url
+::del /f /q *hsts
+::ren filename.txt inumber.txt
 ren wget.exe get.bat
 call mic.bat
