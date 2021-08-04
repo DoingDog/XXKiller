4bbc9fb837142707a954e14dfc15522b7364aba2
ctrl <aqvm@qq.com>
2021-08-04 18:12:59 +0800

    Update mio.bat

--- RMaker/aa/mio.bat
+++ RMaker/aa/mio.bat
@@ -1,19 +1,21 @@
-type prus.bat>mgd.txt
-type i*.txt>>mgd.txt
+::TheRuleMaker
+::Merge
+type prus.dd>mergd.txt
+type i*.txt>>mergd.txt
 del /f /q i*.txt
+::nore
 ren awk.bat gawk.exe
-gawk "!a[$0]++" mgd.txt>what.txt
+gawk "!a[$0]++" mergd.txt>nore.txt
 ren gawk.exe awk.bat
-del /f /q mgd.txt
-set file=what.txt
-(FOR /F "eol=# delims=" %%i in (%file%) do (echo %%i))>seth.txt
-(FOR /F "eol=! delims=" %%i in (seth.txt) do (echo %%i))>%file%
-del /f /q seth.txt
-copy mib.bat+what.txt+mis.bat w.txt
-del /f /q what.txt
-call add.bat
+del /f /q mergd.txt
+::del comments
+(FOR /F "eol=# delims=" %%i in (nore.txt) do (echo %%i))>ktmp.txt
+(FOR /F "eol=! delims=" %%i in (ktmp.txt) do (echo %%i))>nore.txt
+del /f /q ktmp.txt
+::add title and date
+copy title.dd+what.txt+blk.dd w.txt
 echo ! Version: %date%>>tempdate.txt
 echo ! Last modified: %date%T%time%Z>>tempdate.txt
 copy tempdate.txt+w.txt ..\..\w.txt
-del /f /q *.txt
-exit
+::end
+del /f /q *.txt&exit
