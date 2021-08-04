8cfa22f8abf82c1c71403b8c6796ad56b1ccfcd6
ctrl <aqvm@qq.com>
2021-08-04 18:18:51 +0800

    Update mio.bat

--- RMaker/aa/mio.bat
+++ RMaker/aa/mio.bat
@@ -1,21 +1,22 @@
 ::TheRuleMaker
+
 ::Merge
 type prus.dd>mergd.txt
 type i*.txt>>mergd.txt
-del /f /q i*.txt
+
 ::nore
 ren awk.bat gawk.exe
 gawk "!a[$0]++" mergd.txt>nore.txt
 ren gawk.exe awk.bat
-del /f /q mergd.txt
+
 ::del comments
 (FOR /F "eol=# delims=" %%i in (nore.txt) do (echo %%i))>ktmp.txt
-(FOR /F "eol=! delims=" %%i in (ktmp.txt) do (echo %%i))>nore.txt
-del /f /q ktmp.txt
+(FOR /F "eol=! delims=" %%i in (ktmp.txt) do (echo %%i))>nord.txt
+
 ::add title and date
-copy title.dd+what.txt+blk.dd w.txt
-echo ! Version: %date%>>tempdate.txt
-echo ! Last modified: %date%T%time%Z>>tempdate.txt
-copy tempdate.txt+w.txt ..\..\w.txt
+echo ! Version: %date%>>tpdate.txt
+echo ! Last modified: %date%T%time%Z>>tpdate.txt
+copy title.dd+tpdate.txt+nord.txt ..\..\w.txt
+
 ::end
 del /f /q *.txt&exit
