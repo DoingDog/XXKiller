5e572cd0308c30683891d01389a0610fa39badb2
ctrl <aqvm@qq.com>
2021-08-04 17:33:08 +0800

    Update mia.bat

--- RMaker/aa/mia.bat
+++ RMaker/aa/mia.bat
@@ -4,7 +4,6 @@ ren get.bat wget.exe
 
 
 wget -O i1.txt https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
-del /f /q *.html
 wget -O i2.txt https://halflife.coding.net/p/list/d/list/git/raw/master/ad-pc.txt
 wget -O i3.txt https://gitee.com/xinggsf/Adblock-Rule/raw/master/mv.txt
 wget -O i4.txt https://banbendalao.coding.net/p/adgk/d/ADgk/git/raw/master/ADgk.txt
@@ -14,9 +13,10 @@ wget -O i7.txt http://file.trli.club/dns/hosts.txt
 wget -O i8.txt https://anti-ad.net/easylist.txt
 wget -O i9.txt https://raw.githubusercontent.com/BlueSkyXN/AdGuardHomeRules/master/manhua.txt
 
-
 ::新的规则
 ::wget -O fn url
+
+del /f /q *.html
 del /f /q *hsts
 ren wget.exe get.bat
 call mic.bat
