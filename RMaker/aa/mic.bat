e5e2192358eea516ae4d92c16dfaa6e14514bbef
ctrl <aqvm@qq.com>
2021-08-04 17:59:49 +0800

    Update mic.bat

--- RMaker/aa/mic.bat
+++ RMaker/aa/mic.bat
@@ -1,4 +1,19 @@
 type crls.bat>mgd.txt
 type i*.txt>>mgd.txt
 del /f /q i*.txt
-call mio.bat
+ren awk.bat gawk.exe
+gawk "!a[$0]++" mgd.txt>what.txt
+ren gawk.exe awk.bat
+del /f /q mgd.txt
+set file=what.txt
+(FOR /F "eol=# delims=" %%i in (%file%) do (echo %%i))>seth.txt
+(FOR /F "eol=! delims=" %%i in (seth.txt) do (echo %%i))>%file%
+del /f /q seth.txt
+copy mib.bat+what.txt+mis.bat w.txt
+del /f /q what.txt
+call add.bat
+echo ! Version: %date%>>tempdate.txt
+echo ! Last modified: %date%T%time%Z>>tempdate.txt
+copy tempdate.txt+w.txt ..\..\w.txt
+del /f /q *.txt
+exit
