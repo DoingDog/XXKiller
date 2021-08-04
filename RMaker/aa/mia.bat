bde578a72a45e9395eee7ff14675e9bf5936fe47
ctrl <aqvm@qq.com>
2021-08-04 11:09:00 +0800

    Update mia.bat

--- RMaker/aa/mia.bat
+++ RMaker/aa/mia.bat
@@ -1,31 +1,40 @@
 ren get.bat wget.exe
 ::start don
 @echo off
+::p
 wget https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
 del /f /q *hsts
 del /f /q *.html
 ren filter.txt i1.txt
+::p
 wget https://halflife.coding.net/p/list/d/list/git/raw/master/ad-pc.txt
 del /f /q *hsts
 ren ad-pc.txt i2.txt
+::p
 wget https://gitee.com/xinggsf/Adblock-Rule/raw/master/mv.txt
 del /f /q *hsts
 ren mv.txt i3.txt
+::p
 wget https://banbendalao.coding.net/p/adgk/d/ADgk/git/raw/master/ADgk.txt
 ren ADgk.txt i4.txt
 del /f /q *hsts
+::p
 wget https://hacamer.coding.net/p/lite/d/AdBlock-Rules-Mirror/git/raw/master/AdGuard-Simplified-Domain-Names-Filter.txt
 del /f /q *hsts
 ren Ad*.txt i5.txt
+::p
 wget https://neodev.team/lite_adblocker
 del /f /q *hsts
 ren lit* i6.txt
+::p
 wget https://anti-ad.net/easylist.txt
 del /f /q *hsts
 ren easylist.txt i7.txt
+::p
 wget https://raw.githubusercontent.com/BlueSkyXN/AdGuardHomeRules/master/manhua.txt
 del /f /q *hsts
 ren man*.txt i8.txt
+::p
 ::新的规则
 ::wget url
 ::del /f /q *hsts
