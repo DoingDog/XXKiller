d984c6022d96ee51e58f183c7513f7e5439f7eac
Random <aqvm@qq.com>
2021-08-10 14:18:09 +0800

    Update mio.bat

--- RMaker/mio.bat
+++ RMaker/mio.bat
@@ -38,8 +38,8 @@ gawk "!a[$0]++" mergd.txt>nore.txt
 (findstr /r /b "^/." nore.txt)>ntpa.txt
 (findstr /r /v /b "^/." nore.txt)>ntpq.txt
 (findstr /v /b /e "#[^#]*" ntpq.txt)>ntpf.txt
-(findstr /r /v /b "^!." ntpf.txt)>ntpg.txt
-(findstr /r /v /b "^[." ntpg.txt)>nord.txt
+(for /f "eol=! delims=" %%i in (ntpf.txt) do (echo %%i))>ntps.txt
+(for /f "eol=[ delims=" %%i in (ntps.txt) do (echo %%i))>nord.txt
 type ntpa.txt>>nord.txt
 
 ::count rules
