b10f1e1c22cf66fce50dd333233c9b1a578eadd7
Random <aqvm@qq.com>
2021-08-14 22:32:26 +0800

    Create shadow.bat

---
+++ RMaker/shadow.bat
@@ -0,0 +1,14 @@
+@echo off
+cd /d %~dp0\aa
+
+copy frontshadow.dd+final.tx+backshadow.dd mergd.txt
+
+::delete repeated rules
+gawk "!a[$0]++" mergd.txt>nore.txt
+
+::process other lines
+(findstr /v /b /e "#[^#]*" nore.txt)>final.txt
+
+::end cleanup
+copy /y final.txt ..\..\shadow.conf
+del /f /q *.txt&del /f /q final.tx&exit
