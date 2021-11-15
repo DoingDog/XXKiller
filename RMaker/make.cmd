e205c38f4823a1d8e878dda7a4c2e64cde7ce55f
Random <aqvm@qq.com>
2021-11-15 20:07:06 +0800

    Update make.cmd

--- RMaker/make.cmd
+++ RMaker/make.cmd
@@ -8,11 +8,11 @@ wget -O i1.txt https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.tx
 wget -O i2.txt https://raw.githubusercontent.com/o0HalfLife0o/list/master/ad-pc.txt
 wget -O i3.txt https://gitee.com/xinggsf/Adblock-Rule/raw/master/mv.txt
 wget -O i4.txt https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt
-wget -O i5.txt https://file.trli.club/dns/whitelist.txt
+::wget -O i5.txt https://file.trli.club/dns/whitelist.txt
 wget -O i6.txt https://raw.githubusercontent.com/neodevpro/neodevhost/master/lite_adblocker
 ::wget -O i7.txt https://file.trli.club/dns/hosts.txt
 wget -O i8.txt https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-easylist.txt
-wget -O i10.txt https://adaway.org/hosts.txt
+::wget -O i10.txt https://adaway.org/hosts.txt
 wget -O i11.txt https://paulgb.github.io/BarbBlock/blacklists/hosts-file.txt
 wget -O i17.txt https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_plus.txt
 wget -O i12.txt https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-adguard.txt
