26b4218998083d4fa7501c426e57e5cc381dd73a
Random <aqvm@qq.com>
2022-01-28 13:48:52 +0800

    Update make.cmd

--- RMaker/make.cmd
+++ RMaker/make.cmd
@@ -53,72 +53,10 @@ s -u -r -i -o nore.txt mergd.txt
 (findstr /b /c:"~" nore.txt)>>nord.txt
 
 ::numbers
-(findstr /b /c:"0" nore.txt)>>nord.txt
-(findstr /b /c:"1" nore.txt)>>nord.txt
-(findstr /b /c:"2" nore.txt)>>nord.txt
-(findstr /b /c:"3" nore.txt)>>nord.txt
-(findstr /b /c:"4" nore.txt)>>nord.txt
-(findstr /b /c:"5" nore.txt)>>nord.txt
-(findstr /b /c:"6" nore.txt)>>nord.txt
-(findstr /b /c:"7" nore.txt)>>nord.txt
-(findstr /b /c:"8" nore.txt)>>nord.txt
-(findstr /b /c:"9" nore.txt)>>nord.txt
+(findstr /b [0-9] nore.txt)>>nord.txt
 
-::lower case
-(findstr /b /c:"q" nore.txt)>>nord.txt
-(findstr /b /c:"w" nore.txt)>>nord.txt
-(findstr /b /c:"e" nore.txt)>>nord.txt
-(findstr /b /c:"r" nore.txt)>>nord.txt
-(findstr /b /c:"t" nore.txt)>>nord.txt
-(findstr /b /c:"y" nore.txt)>>nord.txt
-(findstr /b /c:"u" nore.txt)>>nord.txt
-(findstr /b /c:"i" nore.txt)>>nord.txt
-(findstr /b /c:"o" nore.txt)>>nord.txt
-(findstr /b /c:"p" nore.txt)>>nord.txt
-(findstr /b /c:"a" nore.txt)>>nord.txt
-(findstr /b /c:"s" nore.txt)>>nord.txt
-(findstr /b /c:"d" nore.txt)>>nord.txt
-(findstr /b /c:"f" nore.txt)>>nord.txt
-(findstr /b /c:"g" nore.txt)>>nord.txt
-(findstr /b /c:"h" nore.txt)>>nord.txt
-(findstr /b /c:"j" nore.txt)>>nord.txt
-(findstr /b /c:"k" nore.txt)>>nord.txt
-(findstr /b /c:"l" nore.txt)>>nord.txt
-(findstr /b /c:"z" nore.txt)>>nord.txt
-(findstr /b /c:"x" nore.txt)>>nord.txt
-(findstr /b /c:"c" nore.txt)>>nord.txt
-(findstr /b /c:"v" nore.txt)>>nord.txt
-(findstr /b /c:"b" nore.txt)>>nord.txt
-(findstr /b /c:"n" nore.txt)>>nord.txt
-(findstr /b /c:"m" nore.txt)>>nord.txt
-
-::upper case
-(findstr /b /c:"Q" nore.txt)>>nord.txt
-(findstr /b /c:"W" nore.txt)>>nord.txt
-(findstr /b /c:"E" nore.txt)>>nord.txt
-(findstr /b /c:"R" nore.txt)>>nord.txt
-(findstr /b /c:"T" nore.txt)>>nord.txt
-(findstr /b /c:"Y" nore.txt)>>nord.txt
-(findstr /b /c:"U" nore.txt)>>nord.txt
-(findstr /b /c:"I" nore.txt)>>nord.txt
-(findstr /b /c:"O" nore.txt)>>nord.txt
-(findstr /b /c:"P" nore.txt)>>nord.txt
-(findstr /b /c:"A" nore.txt)>>nord.txt
-(findstr /b /c:"S" nore.txt)>>nord.txt
-(findstr /b /c:"D" nore.txt)>>nord.txt
-(findstr /b /c:"F" nore.txt)>>nord.txt
-(findstr /b /c:"G" nore.txt)>>nord.txt
-(findstr /b /c:"H" nore.txt)>>nord.txt
-(findstr /b /c:"J" nore.txt)>>nord.txt
-(findstr /b /c:"K" nore.txt)>>nord.txt
-(findstr /b /c:"L" nore.txt)>>nord.txt
-(findstr /b /c:"Z" nore.txt)>>nord.txt
-(findstr /b /c:"X" nore.txt)>>nord.txt
-(findstr /b /c:"C" nore.txt)>>nord.txt
-(findstr /b /c:"V" nore.txt)>>nord.txt
-(findstr /b /c:"B" nore.txt)>>nord.txt
-(findstr /b /c:"N" nore.txt)>>nord.txt
-(findstr /b /c:"M" nore.txt)>>nord.txt
+::alphabet
+(findstr /b [Aa-Zz] nore.txt)>>nord.txt
 
 ::secondary deduplicate and sort
 (gawk "!a[$0]++" nord.txt)>nordn.txt
