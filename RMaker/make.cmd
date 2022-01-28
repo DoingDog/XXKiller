d884ef136314dba2d9db3504aeeda9153c57beee
Random <aqvm@qq.com>
2022-01-28 08:51:41 +0800

    Update make.cmd

--- RMaker/make.cmd
+++ RMaker/make.cmd
@@ -26,8 +26,8 @@ type i*.txt>>mergd.txt
 
 ::delete repeated rules
 ::sort rules Random flag -R
-::s -u -o nore.txt mergd.txt
-(gawk "!a[$0]++" mergd.txt)>nore.txt
+s -u -o nore.txt mergd.txt
+::(gawk "!a[$0]++" mergd.txt)>nore.txt
 
 ::delete comments&rubbish
 (findstr /v /b /c:"# " /c:"[" /c:"!" nore.txt)>nord.txt
