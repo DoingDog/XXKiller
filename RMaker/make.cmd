a8197767b4e6e50809c01c0356c09a5d9c44deea
Random <aqvm@qq.com>
2022-01-28 10:27:07 +0800

    Update make.cmd

--- RMaker/make.cmd
+++ RMaker/make.cmd
@@ -26,15 +26,102 @@ type i*.txt>>mergd.txt
 
 ::delete repeated rules
 ::sort rules Random flag -R
-s -u -r -i -o norew.txt mergd.txt
-(gawk "!a[$0]++" norew.txt)>nore.txt
+s -u -r -i -o nore.txt mergd.txt
 
-::delete comments&rubbish
-(findstr /v /b /c:"# " /c:"[" /c:"!" nore.txt)>nord.txt
-::(gawk "!a[$0]++" nord2.txt)>nord.txt
+::extract
+
+(findstr /b /c:"##" nore.txt)>nord.txt
+(findstr /b /c:"#%#" nore.txt)>>nord.txt
+(findstr /b /c:"#$#" nore.txt)>>nord.txt
+(findstr /b /c:"#@#" nore.txt)>>nord.txt
+(findstr /b /c:"#pkg" nore.txt)>>nord.txt
+(findstr /b /c:"$" nore.txt)>>nord.txt
+(findstr /b /c:"&" nore.txt)>>nord.txt
+(findstr /b /c:"(" nore.txt)>>nord.txt
+(findstr /b /c:"*" nore.txt)>>nord.txt
+(findstr /b /c:"," nore.txt)>>nord.txt
+(findstr /b /c:"-" nore.txt)>>nord.txt
+(findstr /b /c:"." nore.txt)>>nord.txt
+(findstr /b /c:"/" nore.txt)>>nord.txt
+(findstr /b /c:":" nore.txt)>>nord.txt
+(findstr /b /c:";" nore.txt)>>nord.txt
+(findstr /b /c:"=" nore.txt)>>nord.txt
+(findstr /b /c:"?" nore.txt)>>nord.txt
+(findstr /b /c:"@" nore.txt)>>nord.txt
+(findstr /b /c:"_" nore.txt)>>nord.txt
+(findstr /b /c:"^" nore.txt)>>nord.txt
+(findstr /b /c:"|" nore.txt)>>nord.txt
+(findstr /b /c:"~" nore.txt)>>nord.txt
+
+(findstr /b /c:"0" nore.txt)>>nord.txt
+(findstr /b /c:"1" nore.txt)>>nord.txt
+(findstr /b /c:"2" nore.txt)>>nord.txt
+(findstr /b /c:"3" nore.txt)>>nord.txt
+(findstr /b /c:"4" nore.txt)>>nord.txt
+(findstr /b /c:"5" nore.txt)>>nord.txt
+(findstr /b /c:"6" nore.txt)>>nord.txt
+(findstr /b /c:"7" nore.txt)>>nord.txt
+(findstr /b /c:"8" nore.txt)>>nord.txt
+(findstr /b /c:"9" nore.txt)>>nord.txt
+
+(findstr /b /c:"q" nore.txt)>>nord.txt
+(findstr /b /c:"w" nore.txt)>>nord.txt
+(findstr /b /c:"e" nore.txt)>>nord.txt
+(findstr /b /c:"r" nore.txt)>>nord.txt
+(findstr /b /c:"t" nore.txt)>>nord.txt
+(findstr /b /c:"y" nore.txt)>>nord.txt
+(findstr /b /c:"u" nore.txt)>>nord.txt
+(findstr /b /c:"i" nore.txt)>>nord.txt
+(findstr /b /c:"o" nore.txt)>>nord.txt
+(findstr /b /c:"p" nore.txt)>>nord.txt
+(findstr /b /c:"a" nore.txt)>>nord.txt
+(findstr /b /c:"s" nore.txt)>>nord.txt
+(findstr /b /c:"d" nore.txt)>>nord.txt
+(findstr /b /c:"f" nore.txt)>>nord.txt
+(findstr /b /c:"g" nore.txt)>>nord.txt
+(findstr /b /c:"h" nore.txt)>>nord.txt
+(findstr /b /c:"j" nore.txt)>>nord.txt
+(findstr /b /c:"k" nore.txt)>>nord.txt
+(findstr /b /c:"l" nore.txt)>>nord.txt
+(findstr /b /c:"z" nore.txt)>>nord.txt
+(findstr /b /c:"x" nore.txt)>>nord.txt
+(findstr /b /c:"c" nore.txt)>>nord.txt
+(findstr /b /c:"v" nore.txt)>>nord.txt
+(findstr /b /c:"b" nore.txt)>>nord.txt
+(findstr /b /c:"n" nore.txt)>>nord.txt
+(findstr /b /c:"m" nore.txt)>>nord.txt
+
+(findstr /b /c:"Q" nore.txt)>>nord.txt
+(findstr /b /c:"W" nore.txt)>>nord.txt
+(findstr /b /c:"E" nore.txt)>>nord.txt
+(findstr /b /c:"R" nore.txt)>>nord.txt
+(findstr /b /c:"T" nore.txt)>>nord.txt
+(findstr /b /c:"Y" nore.txt)>>nord.txt
+(findstr /b /c:"U" nore.txt)>>nord.txt
+(findstr /b /c:"I" nore.txt)>>nord.txt
+(findstr /b /c:"O" nore.txt)>>nord.txt
+(findstr /b /c:"P" nore.txt)>>nord.txt
+(findstr /b /c:"A" nore.txt)>>nord.txt
+(findstr /b /c:"S" nore.txt)>>nord.txt
+(findstr /b /c:"D" nore.txt)>>nord.txt
+(findstr /b /c:"F" nore.txt)>>nord.txt
+(findstr /b /c:"G" nore.txt)>>nord.txt
+(findstr /b /c:"H" nore.txt)>>nord.txt
+(findstr /b /c:"J" nore.txt)>>nord.txt
+(findstr /b /c:"K" nore.txt)>>nord.txt
+(findstr /b /c:"L" nore.txt)>>nord.txt
+(findstr /b /c:"Z" nore.txt)>>nord.txt
+(findstr /b /c:"X" nore.txt)>>nord.txt
+(findstr /b /c:"C" nore.txt)>>nord.txt
+(findstr /b /c:"V" nore.txt)>>nord.txt
+(findstr /b /c:"B" nore.txt)>>nord.txt
+(findstr /b /c:"N" nore.txt)>>nord.txt
+(findstr /b /c:"M" nore.txt)>>nord.txt
+
+(gawk "!a[$0]++" nord.txt)>nordv.txt
 
 ::count rules
-for /f "tokens=2 delims=:" %%a in ('find /c /v "" nord.txt')do set/a rnum=%%a+1
+for /f "tokens=2 delims=:" %%a in ('find /c /v "" nordv.txt')do set/a rnum=%%a+1
 ::save ct
 echo %rnum%>..\..\ct.txt
 
@@ -47,7 +134,7 @@ echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 type title.dd>w.txt
 type tpdate.txt>>w.txt
-type nord.txt>>w.txt
+type nordv.txt>>w.txt
 type brules.dd>>w.txt
 
 copy /y .\w.txt ..\..\
