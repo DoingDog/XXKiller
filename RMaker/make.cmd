4ce3994a5fe2dc79298f4aa0a3af2252c2ee50ea
Random <aqvm@qq.com>
2022-02-03 16:47:25 +0800

    Update make.cmd

--- RMaker/make.cmd
+++ RMaker/make.cmd
@@ -6,11 +6,12 @@ set LC_ALL='C'
 
 ::start download files in rule-list
 for /f "eol=# tokens=1,2 delims= " %%i in (..\rule-list.ini) do (
-wget --no-hsts -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4860.0 Safari/537.36" --no-check-certificate -t 2 -T 30 -O i%%i.txt %%j
+wget --no-hsts --no-cookies -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4860.0 Safari/537.36" --no-check-certificate -t 2 -T 30 -O i%%i.txt %%j
 )
 
 ::fix encoding to utf8
-for %%i in (i*.txt) do (gb2u8.vbs %%i)
+::if error,enable this
+::for %%i in (i*.txt) do (gb2u8.vbs %%i)
 
 ::delete rubbish files of wget
 ::if exist .\*hsts del /f /q *hsts
