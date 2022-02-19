8963fd3b427309a44f30fec92f62aa6cbbea6c10
Random <aqvm@qq.com>
2022-02-19 16:54:04 +0800

    Update make.cmd

--- RMaker/make.cmd
+++ RMaker/make.cmd
@@ -3,10 +3,12 @@
 chcp 65001
 cd %~dp0\aa
 set LC_ALL='C'
+set listn=0
 
 ::start download files in rule-list
 for /f "eol=# tokens=1,2 delims= " %%i in (..\rule-list.ini) do (
-wget --no-hsts --no-cookies -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4860.0 Safari/537.36" --no-check-certificate -t 2 -T 30 -O i%%i.txt %%j
+set /a listn+=1
+wget --no-hsts --no-cookies -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4860.0 Safari/537.36" --no-check-certificate -t 2 -T 30 -O i%listn%.txt %%j
 )
 
 ::fix encoding to utf8
