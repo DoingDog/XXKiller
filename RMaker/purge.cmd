18472889f7e088e8f4fc8a9338c79f70d73e52a1
Random <koilamshreena@gmail.com>
2022-03-14 18:08:21 +0800

    up

--- RMaker/purge.cmd
+++ RMaker/purge.cmd
@@ -2,7 +2,7 @@
 chcp 65001
 cd %~dp0\aa
 cls
-curl -k -s -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4937.0 Safari/537.36" https://purge.jsdelivr.net/gh/DoingDog/XXKiller@main/w.txt|findstr true&&echo Purge-Failed&&goto :finished
+curl -k -s -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4937.0 Safari/537.36" https://purge.jsdelivr.net/gh/DoingDog/XXKiller@main/w.txt|findstr throttlingRese&&echo Purge-Failed&&goto :finished
 echo Purge-Finished
 :finished
 exit
--- RMaker/readme.cmd
+++ RMaker/readme.cmd
@@ -3,9 +3,17 @@ chcp 65001
 cd %~dp0\aa
 cls
 
-for /f "tokens=* delims=" %%a in (info1.log)do sed -i -E --posix "s/count\:.+$/count:%%a/g" ..\..\readme.md
-for /f "tokens=* delims=" %%a in (info2.log)do sed -i -E --posix "s/version\:.+$/version:%%a/g" ..\..\readme.md
-for /f "tokens=* delims=" %%a in (info3.log)do sed -i -E --posix "s/update\:.+$/update:%%a/g" ..\..\readme.md
-
+for /f "tokens=* delims=" %%a in (info1.log) do (
+    echo %%a
+    sed -i -E --posix "s/count\:.+$/count:%%a/g" ..\..\readme.md
+)
+for /f "tokens=* delims=" %%a in (info2.log) do (
+    echo %%a
+    sed -i -E --posix "s/version\:.+$/version:%%a/g" ..\..\readme.md
+)
+for /f "tokens=* delims=" %%a in (info3.log) do (
+    echo %%a
+    sed -i -E --posix "s/update\:.+$/update:%%a/g" ..\..\readme.md
+)
 del /f /q *.log
 exit
