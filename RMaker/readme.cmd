8776996b5aa62ce36d330ef56c35bd0b01dae541
Random <koilamshreena@gmail.com>
2022-03-14 18:18:39 +0800

    up

--- RMaker/readme.cmd
+++ RMaker/readme.cmd
@@ -5,15 +5,15 @@ cls
 
 for /f "tokens=* delims=" %%a in (info1.log) do (
     echo %%a
-    sed -i -E --posix "s/count\:.+$/count:%%a/g" ..\..\readme.md
+    sed -i -E --posix "s/count:.+$/count:%%a/g" ..\..\README.md
 )
 for /f "tokens=* delims=" %%a in (info2.log) do (
     echo %%a
-    sed -i -E --posix "s/version\:.+$/version:%%a/g" ..\..\readme.md
+    sed -i -E --posix "s/version:.+$/version:%%a/g" ..\..\README.md
 )
 for /f "tokens=* delims=" %%a in (info3.log) do (
     echo %%a
-    sed -i -E --posix "s/update\:.+$/update:%%a/g" ..\..\readme.md
+    sed -i -E --posix "s/update:.+$/update:%%a/g" ..\..\README.md
 )
 del /f /q *.log
 exit
