f81fd42690fce761eccd04764529c5b1ee8ec43a
Random <koilamshreena@gmail.com>
2022-03-14 20:22:53 +0800

    g

--- RMaker/readme.cmd
+++ RMaker/readme.cmd
@@ -1,19 +1,22 @@
 @echo off
 chcp 65001
-cd %~dp0\aa
+cd %~dp0\..
 cls
 
-for /f "tokens=* delims=" %%a in (info1.log) do (
+for /f "tokens=* delims=" %%a in (.\RMaker\aa\info1.log) do (
+    set a=%%a
     echo %%a
-    sed -i -E --posix "s/count:.+$/count:%%a/g" ..\..\README.md
+    sed -i -E --posix "s/count:.+$/count:%a%/g" README.md
 )
-for /f "tokens=* delims=" %%a in (info2.log) do (
+for /f "tokens=* delims=" %%a in (.\RMaker\aa\info2.log) do (
+    set a=%%a
     echo %%a
-    sed -i -E --posix "s/version:.+$/version:%%a/g" ..\..\README.md
+    sed -i -E --posix "s/version:.+$/version:%a%/g" README.md
 )
-for /f "tokens=* delims=" %%a in (info3.log) do (
+for /f "tokens=* delims=" %%a in (.\RMaker\aa\info3.log) do (
+    set a=%%a
     echo %%a
-    sed -i -E --posix "s/update:.+$/update:%%a/g" ..\..\README.md
+    sed -i -E --posix "s/update:.+$/update:%a%/g" README.md
 )
-del /f /q *.log
+del /f /q .\RMaker\aa\*.log
 exit
