62a7ace0670873dfd7ca1ecbde07441ea4467839
Random <aqvm@qq.com>
2022-03-16 20:26:34 +0800

    Update make.cmd

--- RMaker/make.cmd
+++ RMaker/make.cmd
@@ -20,7 +20,7 @@ echo init-OK!
 for /f "eol=# tokens=1,2 delims= " %%i in (..\rule-list.ini) do (
 
 echo Downloading...
-wget -q --no-hsts --no-cookies -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4937.0 Safari/537.36" --no-check-certificate -t 2 -T 30 -O down.txt %%j
+wget -q --no-hsts --local-encoding=UTF-8 --remote-encoding=UTF-8 --restrict-file-names=nocontrol --no-cookies -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4937.0 Safari/537.36" --no-check-certificate -t 2 -T 30 -O down.txt %%j
 echo Download-OK!
 
 sed -i -E --posix "/^$/d" down.txt
