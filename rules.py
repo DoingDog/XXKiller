4c2140340e6ab15f9dc5674ca46c2408fb8a0ee5
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-03-19 06:53:40 +0800

    Create rules.py

---
+++ rules.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+import os
+
+result = []
+ffo = open("w.txt")
+fo = open("w-test.txt", "w")
+result=list(set(ffo.readlines()))
+result.sort()
+fo.writelines(result)
+fo.close()
+ffo.close()
+#os.remove("adblock+adguard.txt")
+#os.rename("adblock+adguard-test.txt","adblock+adguard.txt")
