3eb878e68528d77b09fe7698b50920d09679f36f
0d <aqvm@qq.com>
2022-03-27 17:10:13 +0800

    Update purge.cmd

--- RMaker/purge.cmd
+++ RMaker/purge.cmd
@@ -3,6 +3,6 @@ chcp 65001
 cd %~dp0\aa
 cls
 curl -k -s -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4937.0 Safari/537.36" https://purge.jsdelivr.net/gh/DoingDog/XXKiller@main/w.txt|findstr throttlingRese&&echo Purge-Failed&&goto :finished
-echo Purge-Finished&exit 114514
+echo Purge-Finished&exit 0
 :finished
-exit 0
+exit 114514
